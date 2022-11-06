2da78ce6d2ddc8aab759781956e5b821a4dfeeb6
solovyovsdom <joelborofsky@mac.com>
2022-11-06 14:00:17 -0600

    added useContext for add and delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import RecipeList from './components/RecipeList'
 import './css/app.css'
 
+export const RecipeContext = React.createContext()
+
 function App() {
+  const [recipes, setRecipes] = useState(sampleRecipes)
+
+  const recipeContextValue = {
+    handleRecipeAdd,
+    handleRecipeDelete,
+  }
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [{ id: uuidv4(), name: 'Name', amount: '1 Tbs' }],
+    }
+    setRecipes([...recipes, newRecipe])
+  }
+
+  function handleRecipeDelete(id) {
+    console.log("I'm fucking clicked")
+    setRecipes(recipes.filter((recipe) => recipe.id !== id))
+  }
+
   return (
-    <div>
-      <RecipeList recipes={sampleRecipes} />
-    </div>
+    <RecipeContext.Provider value={recipeContextValue}>
+      <div>
+        <RecipeList recipes={recipes} />
+      </div>
+    </RecipeContext.Provider>
   )
 }
 
@@ -40,13 +70,13 @@ const sampleRecipes = [
       {
         id: 1,
         name: 'Pork',
-        amount: '3 Pounds'
+        amount: '3 Pounds',
       },
       {
         id: 2,
         name: 'Paprika',
-        amount: '2 Tbs'
-      }
+        amount: '2 Tbs',
+      },
     ],
   },
 ]
